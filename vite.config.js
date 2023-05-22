1aa3a39227d120ce25634f265196fa52c893a8cd
Notruche <43958350+Notruche@users.noreply.github.com>
2023-05-22 11:37:59 +0200

    static test 11

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'peko.gribouillos.com',
+  base: '/Pekofolio/',
   plugins: [vue()],
   resolve: {
     alias: {
